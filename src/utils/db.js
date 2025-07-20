5801332462e6e06047610757ddccc9a276c17bdc
sn00k13 <gocitek@gmail.com>
2025-07-20 09:25:39 +0100

    ..

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
+module.exports = pool;
